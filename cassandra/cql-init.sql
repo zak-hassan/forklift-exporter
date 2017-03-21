3f33ff68219452f0d198508975c7d6d861591f42
Zak Hassan <zak.hassan1010@gmail.com>
2017-03-20 22:28:19 -0400

    Adding update to cql file

--- cassandra/cql-init.sql
+++ cassandra/cql-init.sql
@@ -1,17 +1,16 @@
 
 
+
 CREATE KEYSPACE product
   WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
 
-USE KEYSPACE product
-CREATE TABLE  customerOrder (
+CREATE TABLE  product.customerOrder (
  id uuid PRIMARY KEY,
  productName varchar,
  productPrice double,
  productCategory int,
  productQuantity int  );
 
- insert into productfulfillment.customerOrder ( id, productName, productPrice, productCategory, productQuantity) VALUES (now(), 'Bluetooth Headphones', 22.2, 1,2);
+INSERT into product.customerOrder ( id, productName, productPrice, productCategory, productQuantity) VALUES (now(), 'Bluetooth Headphones', 22.2, 1,2);
 
-select * from productfulfillment.customerOrder;
-
+select * from product.customerOrder;
