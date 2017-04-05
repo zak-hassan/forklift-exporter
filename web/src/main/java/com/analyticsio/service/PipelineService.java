d59c89f220ba94ae71bdfd9c165dfa8776cbae27
Zak Hassan <zak.hassan1010@gmail.com>
2017-04-05 12:54:57 -0400

    Adding log4j to project

--- web/src/main/java/com/analyticsio/service/PipelineService.java
+++ web/src/main/java/com/analyticsio/service/PipelineService.java
@@ -3,6 +3,9 @@ package com.analyticsio.service;
 import com.analyticsio.model.Pipeline;
 import com.analyticsio.model.PipelineStatus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,22 +13,28 @@ import java.util.List;
  * Created by zhassan on 2017-04-04.
  */
 public class PipelineService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PipelineService.class);
 
     public List<Pipeline> getAll() {
+        LOGGER.info("getAll Pipeline");
         return Arrays.asList(new Pipeline("file:///home/zhassan/Downloads/rocknroll.parquet?dataformat=parquet", "hdfs://localhost:9000/test5/"));
     }
 
     public PipelineStatus savePipeline(Pipeline pipeline){
+        LOGGER.info("save Pipeline: " + pipeline);
         //TODO: Implement persistance here
         return new PipelineStatus(pipeline,"saved");
     }
 
     public PipelineStatus deletePipeline(String pipeId){
         //TODO: Implement persistance here
+        LOGGER.info("pipeId: " + pipeId );
         return new PipelineStatus(new Pipeline("file:///home/zhassan/Downloads/rocknroll.parquet?dataformat=parquet", "hdfs://localhost:9000/test5/"),"deleted");
     }
 
-    public Pipeline getOnePipeline(){
+    public Pipeline getOnePipeline(String pipeId){
+        LOGGER.info("getOne Pipeline: "+pipeId);
+
         return new Pipeline("file:///home/zhassan/Downloads/rocknroll.parquet?dataformat=parquet", "hdfs://localhost:9000/test5/");
     }
 
